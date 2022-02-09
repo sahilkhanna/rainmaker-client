7b52499da867e4c1a731193ef9a2552bc630999f
Sahil <mail@sahilkhanna.org>
2022-02-09 12:17:18 +1100

    - Can Authenticate - Can get user nodes - Can get time series data

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # rainmaker-client
-A NodeJs Package for interfacing with Espressif Rainmaker IoT
+
+A NodeJs Package for interfacing Espressif Rainmaker IoT.
+
+# Install
+
+```bash
+npm install rainmaker-client
+```
+
+# Usage
+
+```js
+import RainMaker from "rainmaker-client";
+const client = new RainMaker("username", "password");
+
+const SUCCESS = 200;
+
+async function isAuth() {
+  const { status, result } = await client.authenticate();
+  if (status == SUCCESS) {
+    console.log(result);
+  } else {
+    console.log(result);
+  }
+}
+isAuth();
+```
+
+## Authenticate
+
+`authenticate` method is used to get accesstoken for Authnticated APIs.
+
+## User Nodes
+
+`getUserNodes` returns a list of nodes associated to the user. Pass argument true to get details information for the nodes.
+
+## Time Series Data
+
+`getTimeSeriesData` returns time series data for a specific node. Currently it only supports aggregate = `raw`.
+
+# Quick links
+
+- [ESP Rainmaker](https://rainmaker.espressif.com/)
+- [API Definitions for RainMaker Backend Service](https://swaggerapis.rainmaker.espressif.com/)
---
+++ src/index.js
@@ -0,0 +1,89 @@
+import Swagger from "swagger-client";
+const OPENAPI_URL =
+  "https://swaggerapis.rainmaker.espressif.com/Rainmaker_Swagger.yaml";
+
+const requestInterceptor = (request) => {
+  console.log(request);
+  return request;
+};
+class RainMaker {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+    if (this.username === undefined || this.password === undefined) {
+      throw new Error("One or all Credentials are undefined");
+    }
+    this.apiClient = null;
+  }
+  async getApiFunctions() {
+    if (this.apiClient) {
+      return { status: 200, result: this.apiClient.apis };
+    } else {
+      return { status: 400, result: "No Client resolved" };
+    }
+  }
+  async authenticate() {
+    const credentials = {
+      user_name: this.username,
+      password: this.password,
+    };
+    const apiClient = await Swagger({
+      url: OPENAPI_URL,
+      responseContentType: "application/json",
+      authorizations: { AccessToken: "" },
+    });
+    try {
+      const response = await apiClient.apis.User.login(
+        { version: "v1" },
+        {
+          requestBody: credentials,
+        }
+      );
+      this.apiClient = await Swagger({
+        url: OPENAPI_URL,
+        responseContentType: "application/json",
+        authorizations: { AccessToken: response.body.accesstoken },
+      });
+      return { status: response.status, result: "Authentication Successful" };
+    } catch (error) {
+      return { status: error.response.status, result: error.response.body };
+    }
+  }
+  checkClient() {
+    if (!this.apiClient) {
+      return { status: 400, result: "Client Failed" };
+    }
+  }
+  async getUserNodes(detailed = false) {
+    this.checkClient();
+    try {
+      const response = await this.apiClient.apis[
+        "User Node Association"
+      ].getUserNodes({ version: "v1", node_details: detailed });
+      return { status: response.status, result: response.body };
+    } catch (error) {
+      return { status: error.status, result: error.response.body };
+    }
+  }
+  async getTimeSeriesData(node, param, startTime, endTime, next_id, interval) {
+    this.checkClient();
+    try {
+      const response = await this.apiClient.apis["Time Series Data"].GetTSData({
+        version: "v1",
+        node_id: node,
+        param_name: param,
+        type: "float",
+        aggregate: "raw",
+        start_time: startTime,
+        end_time: endTime,
+        start_id: next_id,
+        num_records: interval ? interval : 200,
+      });
+      return { status: response.status, result: response.body };
+    } catch (error) {
+      return { status: error.status, result: error };
+    }
+  }
+}
+
+module.exports = RainMaker;
